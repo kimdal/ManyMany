8aaaed90f14d76ee56964a9ea32f45dec28c147a
kimdal <kimdal1029@naver.com>
2021-11-02 14:45:49 +0900

    add mouseIsPressed

--- sketch.js
+++ sketch.js
@@ -9,8 +9,11 @@ function draw() {
   background(220);
   m.update();
   m.show();
-  
+
   if (mouseIsPressed) {
     //make attract to mouse position
+    let mousePos = createVector(mouseX, mouseY);
+    let f = p5.Vector.sub(mousePos, m.pos);
+    m.applyForce(f);
   }
 }
